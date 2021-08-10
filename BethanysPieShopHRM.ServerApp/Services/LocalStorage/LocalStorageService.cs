8e0780d1b8845038114c606a00a5da1485672300
seangnewman <seangnewman@msn.com>
2021-08-10 16:10:37 -0700

    Add project files.

---
+++ BethanysPieShopHRM.ServerApp/Services/LocalStorage/LocalStorageService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.ServerApp.Services.LocalStorage
+{
+  public class LocalStorageService : ILocalStorageService
+  {
+    public async Task SetItemAsync<T>(string key, T item)
+    {
+      // TODO: Store item in local storage
+    }
+
+    public async Task<T> GetItemAsync<T>(string key)
+    {
+      // TODO: Get item from local storage
+      return default;
+    }
+  }
+}
